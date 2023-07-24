47c9a09c6d1bcb9c3ca4431c35123a808636a0a9
sojin-p <luv2hong@gmail.com>
2023-07-24 20:16:28 +0900

    입력 후 엔터 & 보내기 누르면 레이블에 표시, 알림창 기능 추가

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -10,10 +10,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var inputUIView: UIView!
+    @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var inputTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+    }
+    
+    //입력 후 엔터 & 보내기 버튼 누르면 레이블에 표시
+    @IBAction func enterAndSendButtonTapped(_ sender: Any) {
+        
+        //입력이 1글자보다 작고 nil이면 알림창 띄우고, 그게 아니면 라벨에 넣기
+        if let inputText = inputTextField.text, inputText.count <= 1 {
+            showAlert(title: "내용을 두 글자 이상 입력해 주세요!")
+        } else {
+            resultLabel.text = inputTextField.text
+        }
+        
+    }
+    
+    //알림창
+    func showAlert(title: String) {
+        
+        let alert = UIAlertController(title: title, message: nil, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "확인", style: .cancel)
+        alert.addAction(okButton)
+        present(alert, animated: true)
+
     }
     
     @IBAction func tapGestureTapped(_ sender: UITapGestureRecognizer) {
